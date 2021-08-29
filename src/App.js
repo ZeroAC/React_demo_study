0d1697228bba141455cdc19780ec3ab88b658f04
zeroAc <18041379649@163.com>
2021-08-29 22:58:39 +0800

    add AddTask component to add task in form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 //在react启用emmet提示功能https://medium.com/@eshwaren/enable-emmet-support-for-jsx-in-visual-studio-code-react-f1f5dfe8809c
-import "./App.css";
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
+import './App.css'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask' 
 import { useState } from "react";
 
 function App() {
@@ -37,10 +38,16 @@ function App() {
       )
     );
   };
+  //AddTask
+  const addTask = (task) => {
+      let newTask = {id:Math.floor(Math.random()*1000)+1,...task }
+      setTasks([...tasks,newTask])
+  }
   return (
     <div className="container">
       {/* 注意 必须要包裹在一个父元素内 */}
       <Header />
+      <AddTask onAdd = {addTask}/>
       <Tasks
         tasks={tasks}
         onDeleteTask={deleteTask}
